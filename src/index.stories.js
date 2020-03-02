7425d8c19f0eba9c7b212687620598411ea0ec2c
zivile.utkeviciute <zivile.utkeviciute@agillic.com>
2020-03-02 21:06:42 +0100

    Default story

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,12 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Dropdown from './index'
 
-import HelloWorld from './index'
+const placeholder = 'Select an option'
+
+const options = [
+  {
+    displayName: 'Item1',
+    value: 'Item1'
+  },
+  {
+    displayName: 'Item2',
+    value: 'Item2'
+  },
+  {
+    displayName: 'Item3',
+    value: 'Item3'
+  },
+  {
+    displayName: 'Item4',
+    value: 'Item4'
+  },
+  {
+    displayName: 'Item5',
+    value: 'Item5'
+  },
+  {
+    displayName: 'Item6',
+    value: 'Item6'
+  },
+  {
+    displayName: 'Item7',
+    value: 'Item7'
+  },
+  {
+    displayName: 'Item8',
+    value: 'Item8'
+  },
+  {
+    displayName: 'Item that has a much longer name than anyone thought it would ever will',
+    value: 'LongItem'
+  }
+]
 
 export default {
-  title: 'Hello World',
-  component: HelloWorld,
+  title: 'Default dropdown',
+  component: Dropdown,
 }
 
-export const Default = () => (
-  <HelloWorld />
-)
+export const Default = () => {
+  const [selectedOption, onSetSelectedOption] = useState(null)
+  return (
+    <Dropdown
+      options={options}
+      label='Dropdown label'
+      selectedOption={selectedOption}
+      placeholder={placeholder}
+      onSelectOption={option => {
+        onSetSelectedOption({
+          displayName: option.displayName,
+          value: option.value
+        })
+      }}
+    />
+  )
+}
