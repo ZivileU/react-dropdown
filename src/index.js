3e0a7fdf34707350e23140c6cca4c6c5fb0c8daf
zivile.utkeviciute <zivile.utkeviciute@agillic.com>
2020-03-02 21:07:03 +0100

    Jsx and css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,107 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {string, bool, func, arrayOf, shape, number, oneOfType} from 'prop-types'
+import classNames from 'classnames'
+import AnimateHeight from 'react-animate-height'
 import './index.css'
 
-const HelloWorld = () => (
-  <p className='title'>Hello World</p>
-)
+const Dropdown = ({
+  className,
+  id,
+  label,
+  placeholder,
+  isDisabled,
+  options,
+  isSmall,
+  selectedOption,
+  onSelectOption
+}) => {
+  const selected = selectedOption?.displayName ? 'selected' : ''
+  const {areOptionsVisible, onSetAreOptionsVisible} = useState()
+  return (
+    <div
+      className={classNames(
+        'dropdown',
+        className,
+        {small: isSmall}
+      )}
+      {...(id && ({
+        'data-test-id': id
+      }))}
+    >
+      {label && (
+        <label className='displayLabel'>
+          {label}
+        </label>
+      )}
+      <button
+        {...((!isDisabled && options && (options.length > 0))
+          && {
+            onClick: (() => console.log('click'))
+          }
+        )}
+        type='button'
+        title={selectedOption?.displayName}
+        disabled={isDisabled}
+        className={classNames(
+          'dropdownButton',
+          {selected}
+        )}
+      >
+        <span className='dropdownLabel'>
+          <span>{(selectedOption?.displayName) || placeholder}</span>
+        </span>
+        <i className="fas fa-caret-up"></i>
+      </button>
+      <div className='itemsWrapper'>
+        <AnimateHeight
+          height={areOptionsVisible ? 'auto' : 0}
+          duration={200}
+          easing='ease-in-out'
+        >
+          <ul className='items'>
+            {options?.map(option => (
+              <li key={option.value}>
+                <button
+                  type='button'
+                  title={option.displayName}
+                  className={classNames({
+                    selectedItem: selectedOption?.value === option.value
+                  }, 'item')}
+                  onClick={(() => {
+                    onSelectOption(option)
+                    onSetAreOptionsVisible(false)
+                  })}
+                >
+                  <span>{option.displayName}</span>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </AnimateHeight>
+      </div>
+    </div>
+  )
+}
 
-export default HelloWorld
+Dropdown.propTypes = {
+  className: string,
+  id: string,
+  label: string,
+  placeholder: string,
+  isDisabled: bool,
+  isSmall: bool,
+  areOptionsVisible: bool,
+  options: arrayOf(
+    shape({
+      displayName: string.isRequired,
+      value: oneOfType([string, number]).isRequired
+    }),
+  ).isRequired,
+  selectedOption: shape({
+    displayName: string.isRequired,
+    value: oneOfType([string, number]).isRequired
+  }),
+  onSelectOption: func.isRequired
+}
+
+export default Dropdown
